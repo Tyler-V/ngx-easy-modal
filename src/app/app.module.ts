7138bbb35073a855a115b1f9563a4d24cbfbe390
Tyler Vorpahl <tyler.vorpahl@gmail.com>
2018-02-28 23:37:23 -0500

    Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { EasyModalComponent } from './easy-modal/easy-modal.component';
@@ -9,7 +12,10 @@ import { EasyModalInstanceService } from './easy-modal/easy-modal-instance.servi
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [
     AppComponent,
